064adf2c79fed7174cc0b83e727411215717ee08
bpranava <pranava018@gmail.com>
2025-07-17 22:13:43 +0530

    add google copyright comments

--- internal/tools/kuzudb/kuzudb_test.go
+++ internal/tools/kuzudb/kuzudb_test.go
@@ -1,4 +1,18 @@
-package kuzudb
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kuzudb_test
 
 import (
 	"testing"
@@ -8,6 +22,7 @@ import (
 	"github.com/googleapis/genai-toolbox/internal/server"
 	"github.com/googleapis/genai-toolbox/internal/testutils"
 	"github.com/googleapis/genai-toolbox/internal/tools"
+	"github.com/googleapis/genai-toolbox/internal/tools/kuzudb"
 )
 
 func TestParseFromYamlKuzuDB(t *testing.T) {
@@ -44,7 +59,7 @@ func TestParseFromYamlKuzuDB(t *testing.T) {
 							  field: user_id
 			`,
 			want: server.ToolConfigs{
-				"example_tool": KuzuDBToolConfig{
+				"example_tool": kuzudb.KuzuDBToolConfig{
 					Name:         "example_tool",
 					Kind:         "kuzudb-cypher",
 					Source:       "my-kuzudb-instance",
